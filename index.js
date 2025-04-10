62bb421d2b03a6492c008b2d089455b63947542a
sachins248 <sachin.selvakumar24@gmail.com>
2025-04-09 20:55:10 -0500

    new fact

---
+++ index.js
@@ -0,0 +1,9 @@
+function sachinFunFact() {
+    const participants = [
+        {
+            name: "Sachin",
+            funFact: "I'm a pledge at KTP"
+        },
+    ];
+}
+    sachinFunFact();
